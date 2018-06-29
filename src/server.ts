52550f5eb3b6fd970b4db8342894dd3a7a528d5a
Alejandro Chacon <tsushinkyo@gmail.com>
2018-06-29 11:34:32 +0200

    update package

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import app from './app';
 
-const port = process.env.PORT || 4040;
+const port = process.env.PORT || 5000;
 
 app.listen(port, (err) => {
     if (err) {
         return console.log(err)
     }
-    return console.log(`server is listening on ${port}`)
+    // return console.log(`server is listening on ${port}`)
 })
